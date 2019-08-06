56e55d45ba8f92ea5cdd6b1f040d0d697f81fff5
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-06 18:02:02 +0200

    refactored stat

--- codestat.py
+++ codestat.py
@@ -12,53 +12,58 @@ import json
 from enum import Enum
 
 class Stat:
-    def __init__(self):
+    def __init__(self, n):
+        self.name = n
         self.number_of_structs = 0
         self.number_of_classes = 0
         self.max_indentation = 0
 
+
+def update_stat(s, l):
+    s.number_of_structs += l.number_of_structs
+    s.number_of_classes += l.number_of_classes
+    s.max_indentation = max(s.max_indentation, l.max_indentation)
+
+
 def collect_stat(f):
-    s = Stat()
+    s = Stat(f.name)
     for line in f:
+        l = Stat('')
         if 'class' in line:
-            s.number_of_classes += 1
+            l.number_of_classes = 1
         elif 'struct' in line:
-            s.number_of_structs += 1
+            l.number_of_structs = 1
         if line.strip() != '':
-            indentation = len(line) - len(line.lstrip())
-            s.max_indentation = max(s.max_indentation, indentation)
+            l.max_indentation = len(line) - len(line.lstrip())
+        update_stat(s, l)
     return s
 
 def print_line():
     print('------------')
 
-def parse_file(f):
+
+def print_stat(s):
     print_line()
-    print(f.name)
-    s = collect_stat(f)
+    print(s.name)
     print('structs:', s.number_of_structs)
     print('classes:', s.number_of_classes)
     print('indent:', s.max_indentation)
     print()
-    return s
 
 def main():
     parser = argparse.ArgumentParser(description='Manage my dot files.')
     parser.add_argument('files', type=argparse.FileType('r'), help='files', nargs='+')
     args = parser.parse_args()
 
-    s = Stat()
+    s = Stat('Total')
     for f in args.files:
-        l = parse_file(f)
-        s.number_of_structs += l.number_of_structs
-        s.number_of_classes += l.number_of_classes
+        l = collect_stat(f)
+        print_stat(l)
+        update_stat(s, l)
 
     print()
     print()
-    print_line()
-    print()
-    print('Total number of structs:', s.number_of_structs)
-    print('Total number of classes:', s.number_of_classes)
+    print_stat(s)
 
 
 if __name__ == "__main__":
